eebf2b262a28a7e5eb50e7774a97cb5058578387
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-11 11:32:55 -0500

    build sign up route

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,6 +2,39 @@ const { User } = require("../../models");
 
 const router = require("express").Router();
 
+router.post("/signup", async (req, res) => {
+    try {
+        const usename = await User.findOne({ where: { username: req.body.username } });
+        const email = await User.findOne({ where: { email: req.body.email } });
+        if (usename) {
+            res.status(400).json({ message: "Username is already taken" });
+            return;
+        }
+        if (email) {
+            res.status(400).json({ message: "Email is already taken" });
+            return;
+        }
+        const user = await User.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.usename,
+            password: req.body.password,
+            email: req.body.email,
+            job_title: req.body.job_title,
+            user_img: req.body.user_img
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.user_id = user.id;
+            res.status(200).json({ message: "Logged in" });
+        });
+    }catch(error) {
+        console.log("ERROR occurs while sign up\n", error);
+        res.status(500).json({ message: "Internal error, please try again later" });
+    }
+});
+
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ where: { username: req.body.username } });
@@ -16,9 +49,9 @@ router.post("/login", async (req, res) => {
             req.session.user_id = user.id;
             res.status(200).json({ message: "Logged in" });
         });
-    }catch(error){
+    } catch (error) {
         console.log("ERROR occurs while login\n", error);
-        res.status(500).json({message: "Internal error, please try again later"});
+        res.status(500).json({ message: "Internal error, please try again later" });
     };
 });
 
