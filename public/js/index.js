5704d9f84ab5f778cc51f063ac05852575910be2
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-11 22:03:53 -0500

    build sign up handler

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const loginForm = $(".login-form");
+const signupForm = $(".signup-form");
 
 const logoutBtn = $("#logout-btn");
 
@@ -18,11 +19,10 @@ const loginHandler = async (event) => {
 
         if (res) document.location.replace("/");
     } catch (error) {
-        alert("Failed to login");
+        alert("Username or Password is incorrect");
     }
 
-    $("#username").val("");
-    $("#password").val("");
+    cleanInput();
 };
 
 const logoutHandler = async () => {
@@ -43,7 +43,61 @@ const logoutHandler = async () => {
     }
 };
 
-$(document).ready( () => {
+const signupHandler = async (event) => {
+    event.preventDefault();
+    try {
+        const email = $("#email").val();
+        const username = $("#username").val();
+        const password = $("#password").val();
+        const confirmPassword = $("#confirm-password").val();
+
+        if (password !== confirmPassword) {
+            alert("Password does not match");
+            return;
+        }
+
+        const first_name = $("#first-name").val();
+        const last_name = $("#last-name").val();
+        const job_title = $("#job-title").val();
+
+        const res = await $.ajax({
+            url: "/api/user/signup",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                first_name,
+                last_name,
+                username,
+                password,
+                email,
+                job_title,
+            }),
+            error: (xhr) => {
+                // const res = JSON.parse(xhr);
+                // alert(res);
+                alert(xhr.responseJSON.message);
+            }
+        });
+        if (res) window.location.replace("/");
+    }catch(error){
+        // alert("Fail to sign up");
+        console.log("SIGN UP fails");
+    }
+    cleanInput();
+}
+
+const cleanInput = () => {
+    $("#email").val("");
+    $("#username").val("");
+    $("#password").val("");
+    $("#confirm-password").val("");
+    $("#first-name").val("");
+    $("#last-name").val("");
+    $("#job-title").val("");
+}
+
+$(document).ready(() => {
     loginForm.on("submit", loginHandler);
     logoutBtn.on("click", logoutHandler);
+    signupForm.on("submit", signupHandler);
 });
