9d977f6e0a856cb68df5dacb3477eb87acdeca84
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-13 20:51:36 -0500

    modify job databse

--- seeds/jobdata.js
+++ seeds/jobdata.js
@@ -6,21 +6,18 @@ const jobData = [
         job_description: "This is job description",
         salary: 120_000,
         recruiter_id: 1,
-        user_id: 1
     },
     {
         title: "Software engineer 2",
         job_description: "This is job description",
         salary: 150_000,
         recruiter_id: 1,
-        user_id: 1
     },
     {
         title: "Software engineer 3",
         job_description: "This is job description",
         salary: 150_000,
         recruiter_id: 1,
-        user_id: 1
     },
     {
         title: "Software engineer 4",
