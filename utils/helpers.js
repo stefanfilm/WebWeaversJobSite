d7b92a06346c159e492b1b3c65152e573e47b82a
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-07-14 21:25:39 -0500

    build format date function

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,4 +5,13 @@ function formatSalary(salary) {
     }).format(salary);
 }
 
-module.exports = {formatSalary};
+function formatDate(dateString) { 
+    const date = new Date(dateString);
+    const dd = String(date.getDate()).padStart(2, "0");
+    const mm = String(date.getMonth() + 1).padStart(2, "0");
+    const yyyy = date.getFullYear();
+
+    return `${mm}/${dd}/${yyyy}`;
+}
+
+module.exports = {formatSalary, formatDate};
